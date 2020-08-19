8c0e44d733a601129788c34fd397469ec75f735a
Marieta <maria.it3@gmail.com>
2020-08-19 11:52:44 +0200

    Initial commit

---
+++ src/main/java/es/mariasoria/activitiesrepo/ActivitiesrepoApplication.java
@@ -0,0 +1,13 @@
+package es.mariasoria.activitiesrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiesrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiesrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/mariasoria/activitiesrepo/ActivitiesrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mariasoria.activitiesrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiesrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
