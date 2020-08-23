9fcd012bf2a08cb1a6dba218f54011733e412305
Marieta <maria.it3@gmail.com>
2020-08-23 18:42:04 +0200

    Created list of activities, new activity and details of activity

--- src/main/java/es/mariasoria/activitiesrepo/controller/ActivitiesController.java
+++ src/main/java/es/mariasoria/activitiesrepo/controller/ActivitiesController.java
@@ -5,7 +5,12 @@ import es.mariasoria.activitiesrepo.service.ActivitiesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value="/activities")
@@ -14,36 +19,47 @@ public class ActivitiesController {
     @Autowired
     private ActivitiesService serviceActivity;
 
+    // Displays the list of activities
     @GetMapping("/index")
     public String showIndex(Model model){
         model.addAttribute("activities", serviceActivity.findAll());
         return "activities/index";
     }
 
+    // Displays the view with the details of the activity specified by id
     @GetMapping("/details/{id}")
     public String showActivity(@PathVariable ("id") int idActivity, Model model){
         model.addAttribute("activity", serviceActivity.findById(idActivity));
-        // return la vista detalle
         return "detail";
     }
 
+    // Displays the form to create a new activity
     @GetMapping("/create")
     public String createActivity(){
-        return null;
+        return "activities/formActivity";
     }
 
-    @GetMapping("/save")
-    public String saveActivity(){
+    // Saves the new activity
+    @PostMapping("/save")
+    public String saveActivity(Activity activity, RedirectAttributes attributes){
+        // cuando llegue el objeto activity al controlador
+        // se agregara directamente a nuestra lista
+        serviceActivity.saveActivity(activity);
+        System.out.println("Activity: " + activity);
         // redirect to the index view
-        return null;
+        return "redirect:/activities/index";
     }
 
     @DeleteMapping("/delete")
     public void deleteActivity(){}
 
-
-    @PutMapping("/edit")
-    public void editActivity(){}
+    // Displays the view to edit the activity specified by id
+    @GetMapping("/edit/{id}")
+    public String editActivity(@PathVariable ("id") int idActivity, Model model){
+        model.addAttribute("activity", serviceActivity.findById(idActivity));
+        // add each element of the model to a variable??? filling the form
+        return "activities/editActivity";
+    }
 
 
 }
--- src/main/java/es/mariasoria/activitiesrepo/service/ActivitiesServiceImpl.java
+++ src/main/java/es/mariasoria/activitiesrepo/service/ActivitiesServiceImpl.java
@@ -23,7 +23,18 @@ public class ActivitiesServiceImpl implements ActivitiesService {
         activity1.setGoal("Guessing as many words as possible");
         activity1.setAge("7-10");
         activity1.setCategory("Learning");
-        activity1.setDescription("This game is to have fun. The Hangman");
+        activity1.setDescription("THE HANGMAN - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut cursus nisi. Vivamus sed arcu odio. " +
+                "Aenean pharetra justo eu feugiat blandit. Sed dictum mi vitae nulla aliquam placerat. Aliquam non porttitor elit. Donec aliquam efficitur egestas. " +
+                "Donec ut risus sit amet lorem ultricies ullamcorper vitae at ligula. Aenean id efficitur enim, condimentum porta nisl. Vivamus a erat lorem. " +
+                "Cras elementum bibendum commodo. Mauris id laoreet ex, sed elementum ipsum. Quisque a metus auctor, interdum libero ac, consectetur ex. Praesent " +
+                "facilisis dui lorem, luctus finibus magna lacinia sit amet. Praesent mollis elementum libero, vel rhoncus odio.\n" +
+                "\n" +
+                "Suspendisse lacus quam, varius ultrices ex nec, rhoncus ullamcorper ipsum. Vestibulum at rutrum odio. Praesent convallis arcu in nibh vestibulum, " +
+                "in auctor magna rhoncus. Integer quam dolor, pulvinar at vestibulum et, bibendum at mauris. Mauris finibus nisl urna, elementum suscipit enim " +
+                "efficitur eget. Nullam ornare risus ut tellus volutpat, id egestas eros aliquet. Sed tempor sodales ipsum et vulputate. Integer vitae ligula vel " +
+                "mauris commodo pulvinar. Suspendisse ornare, enim et venenatis elementum, nunc tortor euismod est, luctus pulvinar augue risus sit amet velit. " +
+                "Quisque porttitor orci non ante egestas aliquam. Vestibulum lobortis nibh in ante molestie cursus. Phasellus pulvinar sagittis porta. Morbi nisl ex, " +
+                "varius non erat dictum, blandit rhoncus augue.");
 
         Activity activity2 = new Activity();
         activity2.setId(2);
@@ -32,7 +43,18 @@ public class ActivitiesServiceImpl implements ActivitiesService {
         activity2.setGoal("Finishing the puzzles as soon as possible");
         activity2.setAge("7-10");
         activity2.setCategory("Learning");
-        activity2.setDescription("This game is to have fun. Puzzles");
+        activity2.setDescription("PUZZLES - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut cursus nisi. Vivamus sed arcu odio. " +
+                "Aenean pharetra justo eu feugiat blandit. Sed dictum mi vitae nulla aliquam placerat. Aliquam non porttitor elit. Donec aliquam efficitur egestas. " +
+                "Donec ut risus sit amet lorem ultricies ullamcorper vitae at ligula. Aenean id efficitur enim, condimentum porta nisl. Vivamus a erat lorem. " +
+                "Cras elementum bibendum commodo. Mauris id laoreet ex, sed elementum ipsum. Quisque a metus auctor, interdum libero ac, consectetur ex. Praesent " +
+                "facilisis dui lorem, luctus finibus magna lacinia sit amet. Praesent mollis elementum libero, vel rhoncus odio.\n" +
+                "\n" +
+                "Suspendisse lacus quam, varius ultrices ex nec, rhoncus ullamcorper ipsum. Vestibulum at rutrum odio. Praesent convallis arcu in nibh vestibulum, " +
+                "in auctor magna rhoncus. Integer quam dolor, pulvinar at vestibulum et, bibendum at mauris. Mauris finibus nisl urna, elementum suscipit enim " +
+                "efficitur eget. Nullam ornare risus ut tellus volutpat, id egestas eros aliquet. Sed tempor sodales ipsum et vulputate. Integer vitae ligula vel " +
+                "mauris commodo pulvinar. Suspendisse ornare, enim et venenatis elementum, nunc tortor euismod est, luctus pulvinar augue risus sit amet velit. " +
+                "Quisque porttitor orci non ante egestas aliquam. Vestibulum lobortis nibh in ante molestie cursus. Phasellus pulvinar sagittis porta. Morbi nisl ex, " +
+                "varius non erat dictum, blandit rhoncus augue.");
 
         Activity activity3 = new Activity();
         activity3.setId(3);
@@ -41,7 +63,18 @@ public class ActivitiesServiceImpl implements ActivitiesService {
         activity3.setGoal("Guessing as many words as possible");
         activity3.setAge("7-10");
         activity3.setCategory("Learning");
-        activity3.setDescription("This game is to have fun. Charades");
+        activity3.setDescription("CHARADES - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut cursus nisi. Vivamus sed arcu odio. " +
+                "Aenean pharetra justo eu feugiat blandit. Sed dictum mi vitae nulla aliquam placerat. Aliquam non porttitor elit. Donec aliquam efficitur egestas. " +
+                "Donec ut risus sit amet lorem ultricies ullamcorper vitae at ligula. Aenean id efficitur enim, condimentum porta nisl. Vivamus a erat lorem. " +
+                "Cras elementum bibendum commodo. Mauris id laoreet ex, sed elementum ipsum. Quisque a metus auctor, interdum libero ac, consectetur ex. Praesent " +
+                "facilisis dui lorem, luctus finibus magna lacinia sit amet. Praesent mollis elementum libero, vel rhoncus odio.\n" +
+                "\n" +
+                "Suspendisse lacus quam, varius ultrices ex nec, rhoncus ullamcorper ipsum. Vestibulum at rutrum odio. Praesent convallis arcu in nibh vestibulum, " +
+                "in auctor magna rhoncus. Integer quam dolor, pulvinar at vestibulum et, bibendum at mauris. Mauris finibus nisl urna, elementum suscipit enim " +
+                "efficitur eget. Nullam ornare risus ut tellus volutpat, id egestas eros aliquet. Sed tempor sodales ipsum et vulputate. Integer vitae ligula vel " +
+                "mauris commodo pulvinar. Suspendisse ornare, enim et venenatis elementum, nunc tortor euismod est, luctus pulvinar augue risus sit amet velit. " +
+                "Quisque porttitor orci non ante egestas aliquam. Vestibulum lobortis nibh in ante molestie cursus. Phasellus pulvinar sagittis porta. Morbi nisl ex, " +
+                "varius non erat dictum, blandit rhoncus augue.");
 
         Activity activity4 = new Activity();
         activity4.setId(4);
@@ -50,7 +83,18 @@ public class ActivitiesServiceImpl implements ActivitiesService {
         activity4.setGoal("Reply correctly the questions before the balloon explodes");
         activity4.setAge("7-10");
         activity4.setCategory("Learning");
-        activity4.setDescription("This game is to have fun. Hot potato");
+        activity4.setDescription("HOT POTATO - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut cursus nisi. Vivamus sed arcu odio. " +
+                "Aenean pharetra justo eu feugiat blandit. Sed dictum mi vitae nulla aliquam placerat. Aliquam non porttitor elit. Donec aliquam efficitur egestas. " +
+                "Donec ut risus sit amet lorem ultricies ullamcorper vitae at ligula. Aenean id efficitur enim, condimentum porta nisl. Vivamus a erat lorem. " +
+                "Cras elementum bibendum commodo. Mauris id laoreet ex, sed elementum ipsum. Quisque a metus auctor, interdum libero ac, consectetur ex. Praesent " +
+                "facilisis dui lorem, luctus finibus magna lacinia sit amet. Praesent mollis elementum libero, vel rhoncus odio.\n" +
+                "\n" +
+                "Suspendisse lacus quam, varius ultrices ex nec, rhoncus ullamcorper ipsum. Vestibulum at rutrum odio. Praesent convallis arcu in nibh vestibulum, " +
+                "in auctor magna rhoncus. Integer quam dolor, pulvinar at vestibulum et, bibendum at mauris. Mauris finibus nisl urna, elementum suscipit enim " +
+                "efficitur eget. Nullam ornare risus ut tellus volutpat, id egestas eros aliquet. Sed tempor sodales ipsum et vulputate. Integer vitae ligula vel " +
+                "mauris commodo pulvinar. Suspendisse ornare, enim et venenatis elementum, nunc tortor euismod est, luctus pulvinar augue risus sit amet velit. " +
+                "Quisque porttitor orci non ante egestas aliquam. Vestibulum lobortis nibh in ante molestie cursus. Phasellus pulvinar sagittis porta. Morbi nisl ex, " +
+                "varius non erat dictum, blandit rhoncus augue.");
 
         list.add(activity1);
         list.add(activity2);
